2a25aea3f6c85998b12272d053263e29d89d7793
Dylan <dylan.patrick.cheng@gmail.com>
2017-02-01 15:58:56 -0800

    finish card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,7 +1,31 @@
 class Card
 
-  def initialize(name, suit)
+  VALUES = {
+    :two => 2,
+    :three => 3,
+    :four => 4,
+    :five => 5,
+    :six => 6,
+    :seven => 7,
+    :eight => 8,
+    :nine => 9,
+    :ten => 10,
+    :jack => 11,
+    :queen => 12,
+    :king => 13,
+    :ace => 14
+  }
 
+  attr_reader :name, :suit, :value
+
+  def initialize(name, suit)
+    @name = name
+    @suit = suit
+    @value = read_value
+  end
+
+  def read_value
+    VALUES[@name]
   end
 
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,14 +2,14 @@ require "card"
 
 describe Card do
 
-  subject(:card) { Card.new("king", :diamond)}
+  subject(:card) { Card.new(:king, :diamond)}
 
 
   describe "#initialize" do
 
     it "is provided name and suit" do
       expect(card.suit).to eq(:diamond)
-      expect(card.name).to eq("king")
+      expect(card.name).to eq(:king)
     end
 
     it "correctly establishes a value" do
@@ -20,9 +20,9 @@ describe Card do
 
   describe "#read_value" do
 
-    let(:queen) { Card.new("queen", :club) }
-    let(:seven) { Card.new("seven", :heart) }
-    let(:jack) { Card.new("jack", :spade) }
+    let(:queen) { Card.new(:queen, :club) }
+    let(:seven) { Card.new(:seven, :heart) }
+    let(:jack) { Card.new(:jack, :spade) }
 
     it "converts from name to value" do
       expect(queen.read_value).to eq(12)
